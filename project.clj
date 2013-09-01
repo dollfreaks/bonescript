d533fefbec4f7b0ae87e11e57d23cd20940ead92
Darrell Hamilton <darrell.noice@gmail.com>
2013-09-01 01:26:10 -0700

    little bit of cljs noobery and just plain derping around

--- project.clj
+++ project.clj
@@ -6,7 +6,15 @@
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/core.async "0.1.0-SNAPSHOT"]
                  [org.clojure/clojurescript "0.0-1859"]]
-  
+
   :repositories {"sonatype" "https://oss.sonatype.org/content/groups/public/"
                  "sonatype-staging" "https://oss.sonatype.org/content/groups/staging/"}
-  )
+
+  :plugins [[lein-cljsbuild "0.3.2"]]
+
+  :cljsbuild {
+              :builds [{:source-paths ["src"]
+                        :compiler {:optimizations :simple
+                                   :pretty-print true
+                                   :target :nodejs}
+                        }]})
--- src/com/dollfreaks/bonescript/core.cljs
+++ src/com/dollfreaks/bonescript/core.cljs
@@ -1,84 +1,86 @@
 (ns com.dollfreaks.bonescript.core
-  (:require [cljs.core.async :refer (chan >!!)]
-            [clsj.nodejs :as node]))
+  (:require [cljs.nodejs :as node]
+            [cljs.core.async :refer (chan >!! <!!)]))
 
-(def b (node/require "bonescript"))
+(let [b (cljs.nodejs/require "bonescript")]
+  (defn get-platform 
+    ([] (get-platform (chan)))
+    ([ch]
+     (.getPlatform b #(>!! ch %))
+     ch))
 
-(defn get-platform 
-  ([] (get-platform (chan)))
-  ([ch]
-   (b/getPlatform #(>!! ch %))
-   ch))
+  (defn set-pin-mode!
+    ([opts] (set-pin-mode! opts (chan)))
+    ([{pin :pin
+       direction :direction
+       mux :mux
+       pullup :pullup
+       slew :slew}
+      ch]
+     (apply #(.pinMode b %) [pin direction mux pullup slew #(>!! ch %)])
+     ch))
 
-(defn set-pin-mode!
-  ([opts] (pin-mode opts (chan)))
-  ([{pin :pin
-     direction :direction
-     mux :mux
-     pullup :pullup
-     slew :slew}
-    ch]
-   (apply b/pinMode [pin direction mux pullup slew #(>!! ch %)])
-   ch))
+  (defn get-pin-mode
+    ([p] (get-pin-mode p (chan)))
+    ([p ch]
+     (.getPinMode b p #(>!! ch %))
+     ch))
 
-(defn get-pin-mode
-  ([p] (get-pin-mode p (chan)))
-  ([p ch]
-   (b/getPinMode p #(>!! ch %))
-   ch))
+  (defn digital-write!
+    ([p v] (digital-write! p v (chan)))
+    ([p v ch]
+     (.digitalWrite b p v #(>!! ch %))
+     ch))
 
-(defn digital-write!
-  ([p v] (digital-write! p v (chan)))
-  ([p v ch]
-   (b/digitalWrite p v #(>!! ch %))
-   ch))
-
-(defn digital-read
-  ([p] (digital-read [p (chan)]))
-  ([p ch]
-   (b/digitalRead p #(>!! ch %))
-   ch))
-
-
-(defn analog-write!
-  ([p v hz] (analog-write! p v hz (chan)))
-  ([p v hz ch]
-   (b/analogWrite p v hz #(>!! ch %))
-   ch))
-
-(defn analog-read
-  ([p] (analog-read [p (chan)]))
-  ([p ch]
-   (b/digitalRead p #(>!! ch %))
-   ch))
-
-(defn attach-interrupt!
-  ([p h m] (attach-interrupt p h m (chan)))
-  ([p h m ch]
-   (b/attachInterrupt p h m #(>!! ch %))
-   ch))
-
-(defn detach-interrupt!
-  ([p] (deftach-interrupt! p (chan)))
-  ([p ch]
-   (b/detachInterrupt p #(>!! ch %))))
-
-(defn read-text-file
-  ([p] (read-file p (chan)))
-  ([p ch]
-   (b/readTextFile p #(>!! ch %))
-   ch))
-
-(defn write-text-file!
-  ([p d] (write-file p d (chan)))
-  ([p d ch]
-   (b/writeTextFile p d #(>!! ch %))
-   ch))
-
-(defn shift-out!
-  ([dp cp order v] (shift-out dp cp order v (chan)))
-  ([dp cp order v ch]
-   (b/shiftOut dp cp order v #(>!! ch %))
-   ch))
+  (defn digital-read
+    ([p] (digital-read [p (chan)]))
+    ([p ch]
+     (.digitalRead b p #(>!! ch %))
+     ch))
 
 
+  (defn analog-write!
+    ([p v hz] (analog-write! p v hz (chan)))
+    ([p v hz ch]
+     (.analogWrite b p v hz #(>!! ch %))
+     ch))
+
+  (defn analog-read
+    ([p] (analog-read [p (chan)]))
+    ([p ch]
+     (.digitalRead b p #(>!! ch %))
+     ch))
+
+  (defn attach-interrupt!
+    ([p h m] (attach-interrupt! p h m (chan)))
+    ([p h m ch]
+     (.attachInterrupt b p h m #(>!! ch %))
+     ch))
+
+  (defn detatch-interrupt!
+    ([p] (detatch-interrupt! p (chan)))
+    ([p ch]
+     (.detachInterrupt b p #(>!! ch %))))
+
+  (defn read-text-file
+    ([p] (read-text-file p (chan)))
+    ([p ch]
+     (.readTextFile b p #(>!! ch %))
+     ch))
+
+  (defn write-text-file!
+    ([p d] (write-text-file! p d (chan)))
+    ([p d ch]
+     (.writeTextFile b p d #(>!! ch %))
+     ch))
+
+  (defn shift-out!
+    ([dp cp order v] (shift-out! dp cp order v (chan)))
+    ([dp cp order v ch]
+     (.shiftOut b dp cp order v #(>!! ch %))
+     ch)))
+
+(defn start [& _]
+  (println (<!! (get-platform (chan)))))
+
+(set! *main-cli-fn* start)
