c6dc0a56d9b8da20195b67ede63f4e0865050ce6
Darrell Hamilton <darrell.noice@gmail.com>
2013-09-01 22:31:15 -0700

    actually writing cljs instead of clj...

--- src/com/dollfreaks/bonescript/core.cljs
+++ src/com/dollfreaks/bonescript/core.cljs
@@ -1,86 +1,91 @@
 (ns com.dollfreaks.bonescript.core
   (:require [cljs.nodejs :as node]
-            [cljs.core.async :refer (chan >!! <!!)]))
+            [cljs.core.async :refer (chan >! <!)])
+  (:require-macros [cljs.core.async.macros :refer (go)]))
 
-(let [b (cljs.nodejs/require "bonescript")]
+(let [b (cljs.nodejs/require "bonescript")
+      send-message (fn [ch] #(go (apply >! ch %)))]
+
   (defn get-platform 
-    ([] (get-platform (chan)))
+    ([] (get-platform (chan 1)))
     ([ch]
-     (.getPlatform b #(>!! ch %))
+     (.getPlatform b (send-message ch))
      ch))
 
   (defn set-pin-mode!
-    ([opts] (set-pin-mode! opts (chan)))
+    ([opts] (set-pin-mode! opts (chan 1)))
     ([{pin :pin
        direction :direction
        mux :mux
        pullup :pullup
        slew :slew}
       ch]
-     (apply #(.pinMode b %) [pin direction mux pullup slew #(>!! ch %)])
+     (apply #(.pinMode b %) [pin direction mux pullup slew (send-message ch)])
      ch))
 
   (defn get-pin-mode
-    ([p] (get-pin-mode p (chan)))
+    ([p] (get-pin-mode p (chan 1)))
     ([p ch]
-     (.getPinMode b p #(>!! ch %))
+     (.getPinMode b p (send-message ch))
      ch))
 
   (defn digital-write!
-    ([p v] (digital-write! p v (chan)))
+    ([p v] (digital-write! p v (chan 1)))
     ([p v ch]
-     (.digitalWrite b p v #(>!! ch %))
+     (.digitalWrite b p v (send-message ch))
      ch))
 
   (defn digital-read
-    ([p] (digital-read [p (chan)]))
+    ([p] (digital-read [p (chan 1)]))
     ([p ch]
-     (.digitalRead b p #(>!! ch %))
+     (.digitalRead b p (send-message ch))
      ch))
 
 
   (defn analog-write!
-    ([p v hz] (analog-write! p v hz (chan)))
+    ([p v hz] (analog-write! p v hz (chan 1)))
     ([p v hz ch]
-     (.analogWrite b p v hz #(>!! ch %))
+     (.analogWrite b p v hz (send-message ch))
      ch))
 
   (defn analog-read
-    ([p] (analog-read [p (chan)]))
+    ([p] (analog-read [p (chan 1)]))
     ([p ch]
-     (.digitalRead b p #(>!! ch %))
+     (.digitalRead b p (send-message ch))
      ch))
 
   (defn attach-interrupt!
-    ([p h m] (attach-interrupt! p h m (chan)))
+    ([p h m] (attach-interrupt! p h m (chan 1)))
     ([p h m ch]
-     (.attachInterrupt b p h m #(>!! ch %))
+     (.attachInterrupt b p h m (send-message ch))
      ch))
 
   (defn detatch-interrupt!
-    ([p] (detatch-interrupt! p (chan)))
+    ([p] (detatch-interrupt! p (chan 1)))
     ([p ch]
-     (.detachInterrupt b p #(>!! ch %))))
+     (.detachInterrupt b p (send-message ch))))
 
   (defn read-text-file
-    ([p] (read-text-file p (chan)))
+    ([p] (read-text-file p (chan 1)))
     ([p ch]
-     (.readTextFile b p #(>!! ch %))
+     (.readTextFile b p (send-message ch))
      ch))
 
   (defn write-text-file!
-    ([p d] (write-text-file! p d (chan)))
+    ([p d] (write-text-file! p d (chan 1)))
     ([p d ch]
-     (.writeTextFile b p d #(>!! ch %))
+     (.writeTextFile b p d (send-message ch))
      ch))
 
   (defn shift-out!
-    ([dp cp order v] (shift-out! dp cp order v (chan)))
+    ([dp cp order v] (shift-out! dp cp order v (chan 1)))
     ([dp cp order v ch]
-     (.shiftOut b dp cp order v #(>!! ch %))
+     (.shiftOut b dp cp order v (send-message ch))
      ch)))
 
 (defn start [& _]
-  (println (<!! (get-platform (chan)))))
+  (go
+    (let [ch (chan 1)]
+      (println (<! (get-platform ch))))))
 
 (set! *main-cli-fn* start)
