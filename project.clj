042f8a2ac5a9c23391e74b6668cfaa67b536f2bc
Darrell Hamilton <darrell.noice@gmail.com>
2013-09-01 22:24:47 -0700

    google closure!

--- project.clj
+++ project.clj
@@ -3,9 +3,11 @@
   :url "https://github.com/dollfreaks/bonescript"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/core.async "0.1.0-SNAPSHOT"]
-                 [org.clojure/clojurescript "0.0-1859"]]
+                 [org.clojure/clojurescript "0.0-1859"]
+                 [org.clojure/google-closure-library-third-party "0.0-2029-2"]]
 
   :repositories {"sonatype" "https://oss.sonatype.org/content/groups/public/"
                  "sonatype-staging" "https://oss.sonatype.org/content/groups/staging/"}
