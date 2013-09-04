45b1737faddbd723264f0db1553e02d2bebc3f80
Darrell Hamilton <darrell.noice@gmail.com>
2013-09-04 16:51:52 -0700

    merged core and interop to interop

--- src/com/dollfreaks/bonescript/interop.cljs
+++ src/com/dollfreaks/bonescript/interop.cljs
@@ -1,17 +1,89 @@
-(ns com.dollfreaks.bonescript.interop
-  (:require [cljs.nodejs :as node]))
+(ns com.dollfreaks.bonescript.core
+  (:require [cljs.nodejs :as node]
+            [cljs.core.async :refer (chan >! <!)])
+  (:require-macros [cljs.core.async.macros :refer (go)]))
 
-(let [b (node/require "bonescript")]
+(let [b (cljs.nodejs/require "bonescript")
+      callback-to-chan (fn [ch] (fn [& args] (go (>! ch args))))]
+
   (defn to-direction [d]
     (case d
       :output (aget b "OUTPUT")
       :input (aget b "INPUT")
       :pullup (aget b "INPUT_PULLUP")))
 
-  (defn to-pin-name [p]
-    (name p))
+  (defn to-pin-name [p] (name p))
  
   (def HIGH (aget b "HIGH"))
   (def LOW (aget b "LOW"))
-  
-  )
+
+  (defn get-platform 
+    ([] (get-platform (chan 1)))
+    ([ch]
+     (.getPlatform b (callback-to-chan ch))
+     ch))
+
+  ; gonna need some work to support the "returns a channel" model
+  (defn set-pin-mode!
+    [p mode]
+      (.pinMode b p mode))
+
+  (defn get-pin-mode
+    ([p] (get-pin-mode p (chan 1)))
+    ([p ch]
+     (.getPinMode b p (callback-to-chan ch))
+     ch))
+
+  (defn digital-write!
+    ([p v] (digital-write! p v (chan 1)))
+    ([p v ch]
+     (.digitalWrite b p v (callback-to-chan ch))
+     ch))
+
+  (defn digital-read
+    ([p] (digital-read [p (chan 1)]))
+    ([p ch]
+     (.digitalRead b p (callback-to-chan ch))
+     ch))
+
+
+  (defn analog-write!
+    ([p v hz] (analog-write! p v hz (chan 1)))
+    ([p v hz ch]
+     (.analogWrite b p v hz (callback-to-chan ch))
+     ch))
+
+  (defn analog-read
+    ([p] (analog-read [p (chan 1)]))
+    ([p ch]
+     (.digitalRead b p (callback-to-chan ch))
+     ch))
+
+  (defn attach-interrupt!
+    ([p h m] (attach-interrupt! p h m (chan 1)))
+    ([p h m ch]
+     (.attachInterrupt b p h m (callback-to-chan ch))
+     ch))
+
+  (defn detatch-interrupt!
+    ([p] (detatch-interrupt! p (chan 1)))
+    ([p ch]
+     (.detachInterrupt b p (callback-to-chan ch))))
+
+  (defn read-text-file
+    ([p] (read-text-file p (chan 1)))
+    ([p ch]
+     (.readTextFile b p (callback-to-chan ch))
+     ch))
+
+  (defn write-text-file!
+    ([p d] (write-text-file! p d (chan 1)))
+    ([p d ch]
+     (.writeTextFile b p d (callback-to-chan ch))
+     ch))
+
+  (defn shift-out!
+    ([dp cp order v] (shift-out! dp cp order v (chan 1)))
+    ([dp cp order v ch]
+     (.shiftOut b dp cp order v (callback-to-chan ch))
+     ch)))
