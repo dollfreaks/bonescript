c38290267a7d6f57fcd4bba2ea90a396b13fe671
Darrell Hamilton <darrell.noice@gmail.com>
2013-09-05 00:55:56 -0700

    a little bit of reorg and a more complete implementation of pin-has-value?

--- src/com/dollfreaks/bonescript/io/digital.cljs
+++ src/com/dollfreaks/bonescript/io/digital.cljs
@@ -1,36 +1,48 @@
 (ns com.dollfreaks.bonescript.io.digital
-  (:require [cljs.nodejs :as node]
-            [com.dollfreaks.bonescript.interop :as iop]
-            [cljs.core.async :refer (<!)])
-  (:require-macros [cljs.core.async.macros (go)]))
+  (:require [com.dollfreaks.bonescript.interop :as iop]
+            [cljs.core.async :refer (<! >! chan close!)])
+  (:require-macros [cljs.core.async.macros :refer (go)]))
 
-(let [b (node/require "bonescript")]
-  (defn write!
-    [{pin :pin mode :mode dir :direction :as p} state ch]
-    (let [p-name (iop/to-pin-name pin)]
-      (if (and (= :digital mode) (= :output dir))
-        (do
-          ; is there a performance penalty to setting the pin mode every time?
-          (iop/set-pin-mode! p-name (iop/to-direction dir))
-          (iop/digital-write! p-name state ch))
-        ; throw an exception?
-        (println "cannot write to an input or non-digital pin") 
-        )))
+(defn write!
+  [{pin :pin mode :mode dir :direction :as p} state ch]
+  (let [p-name (iop/to-pin-name pin)]
+    (if (and (= :digital mode) (= :output dir))
+      (do
+        ; is there a performance penalty to setting the pin mode every time?
+        (iop/set-pin-mode! p-name (iop/to-direction dir))
+        (iop/digital-write! p-name state ch))
+      ; throw an exception?
+      (println "cannot write to an input or non-digital pin")
+      )))
 
-  (defn read 
-    [{pin :pin mode :mode dir :direction :as p} ch]
+(defn read
+  [{pin :pin mode :mode dir :direction :as p} ch]
+  (let [p-name (iop/to-pin-name pin)]
+    (if (and (= :digital mode) (not (= :output dir)))
+      (do
+        ; is there a performance penalty to setting the pin mode every time?
+        (iop/set-pin-mode! p-name (iop/to-direction dir))
+        (iop/digital-read p-name ch))
+      ; throw an exception?
+      (println "cannot read from an output or non-digital pin")
+      )))
 
-    (let [p-name (iop/to-pin-name pin)]
-      (if (and (= :digital mode) (not (= :output dir)))
-        (do
-          ; is there a performance penalty to setting the pin mode every time?
-          (iop/set-pin-mode! p-name (iop/to-direction dir))
-          (iop/digital-read p-name ch))
-        ; throw an exception?
-        (println "cannot read from an output or non-digital pin") 
-        )))
+(defn set-high!
+  ([p] (set-high! p (chan 1)))
+  ([p ch] (write! p iop/HIGH ch)))
 
-  (defn set-high! [p ch] (write! p iop/HIGH ch))
-  (defn set-low! [p ch] (write! p iop/LOW ch))
-  (defn high? [p ch] (= iop/HIGH (digital-read-from-pin p iop/HIGH)))
-  (defn low? [p ch] (digital-read-from-pin p iop/LOW)))
+(defn set-low!
+  ([p] (set-low! p (chan 1)))
+  ([p ch] (write! p iop/LOW ch)))
+
+(defn pin-has-value? [p v ch]
+  (let [temp-chan (chan 1)]
+    (go
+      (>! ch (= v (:value (<! (iop/digital-read-from-pin p temp-chan)))))
+      (close! temp-chan)) ch))
+
+(defn high? [p ch]
+  (pin-has-value? p iop/HIGH ch))
+
+(defn low? [p ch]
+  (pin-has-value? p iop/LOW ch))
